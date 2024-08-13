8767886ab4a20d1f5f88ca6baf57e3b833137bb6
Ivan Rosa <Ivan Rosa@DESKTOP-2VJVQ61>
2024-08-13 12:52:33 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio294030347/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio294030347
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ejercicio294030347/Numero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Ejercicio294030347
+{
+
+    [Table("numero")]
+    public class Numero
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("valor")]
+        public int Valor { get; set; }
+
+        [Column("signo")]
+        public string? Signo { get; set; }
+    }
+}
